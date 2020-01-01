e931ea2ceaa44a3ea900f42dcdb19c1e2720f33e
Flávio Santos <flaviosantos.dev@gmail.com>
2020-01-01 10:09:41 -0300

    Scrapper agora usa XPath

--- app/Library/Scrapper.php
+++ app/Library/Scrapper.php
@@ -10,6 +10,7 @@ namespace app\Library;
 
 
 
+use App\Marca;
 use App\Produto;
 
 class Scrapper
@@ -19,19 +20,40 @@ class Scrapper
         return $produto;
     }
 
-
     public static function getProduto($codigo_barras){
+        libxml_use_internal_errors(true);
+        $dom = new \DOMDocument();
+        $dom->loadHTMLFile('https://cosmos.bluesoft.com.br/produtos/' . $codigo_barras);
+        $xpath = new \DOMXPath($dom);
+        $nodes = $xpath->query("//h1[@class='page-header']/text()");
+        $produto = new Produto();
+        $produto->nome = $nodes[0]->nodeValue;
+
+        $produto->codigo_barras = $codigo_barras;
+        libxml_use_internal_errors(false);
+        $produto->save();
+
+        return $produto;
+    }
+
+    public static function getProdutoo($codigo_barras){
         $html = file_get_contents('https://cosmos.bluesoft.com.br/produtos/' . $codigo_barras);
         $produto = new Produto;
         preg_match("/e>.+- GTIN/", $html,         $nome);
-        // preg_match("/[0-9]{13}/", $html, $codigo_barras);
 
         $produto->nome = substr($nome[0], 2, -7);
         $produto->nome = ucfirst($produto->nome);
         $produto->codigo_barras = $codigo_barras;
+        $marca = self::getMarca($html);
 
         $produto->save();
 
         return $produto;
+    }
+
+    public static function getMarca($html){
+     /*   $marca = new Marca;
+        preg_match("")
+        $marca->nome =*/
     }
 }
