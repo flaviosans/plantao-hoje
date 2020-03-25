ec6246f9a0a95f682aaa128d22ad8daea618e861
Flávio Santos <flaviosantos.dev@gmail.com>
2020-03-25 18:03:23 -0300

    Scrapper busca produtos e marcas.

--- app/Library/Scrapper.php
+++ app/Library/Scrapper.php
@@ -17,29 +17,41 @@ class Scrapper
 {
     public static function scrap($codigo_barras){
         $xpath = self::getDomXPath('https://cosmos.bluesoft.com.br/produtos/' . $codigo_barras);
+        self::buscarImagem($xpath);
+        return self::buscarProduto($xpath, $codigo_barras);
 
-        return self::buscarProduto($xpath);
     }
 
-    private static function buscarProduto($xpath){
+    private static function buscarProduto($xpath, $codigo_barras){
         $node_nome = $xpath->query("//h1[@class='page-header']/text()");
-
         $produto = new Produto();
         $produto->nome = $node_nome[0]->nodeValue;
         $produto->codigo_barras = $codigo_barras;
+        $produto->marca()->associate(self::buscarMarca($xpath));
         $produto->save();
 
         return $produto;
     }
-    public static function getDomXPath($filename){
-        libxml_use_internal_errors(true);
-        $dom = new \DOMDocument();
-        $dom->loadHTMLFile($filename);
-        libxml_use_internal_errors(false);
-        return new \DOMXPath($dom);
+
+    public static function buscarMarca($xpath){
+        $node_nome = $xpath->query("//span[@class='brand-name']/a/text()");
+        $marca = Marca::firstOrCreate([
+            'nome' => $node_nome[0]->nodeValue
+        ]);
+
+        $marca->save();
+        return $marca;
     }
 
-    public static function buscarMarca(){
+    private static function buscarImagem($xpath){
+    }
 
+    private static function getDomXPath($url){
+        libxml_use_internal_errors(true);
+        $dom = new \DOMDocument();
+        $file = file_get_contents($url);
+        $dom->loadHTML( '<?xml encoding="UTF-8">' . $file);
+        libxml_use_internal_errors(false);
+        return new \DOMXPath($dom);
     }
 }
