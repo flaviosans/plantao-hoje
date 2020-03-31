8b31eaa21f9ba7ee718e5b3b3fc2905dc397d6d6
Flávio Santos <flaviosantos.dev@gmail.com>
2020-03-31 17:40:45 -0300

    Organiza o pluralizer personalizado

--- app/Providers/PluralizarionServiceProvider.php
+++ app/Providers/PluralizarionServiceProvider.php
@@ -4,14 +4,15 @@ namespace App\Providers;
 
 use Doctrine\Common\Inflector\Inflector;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Pluralizer;
 
 class PluralizarionServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        Inflector::rules('plural', ['irregular' => ['cotacao' => 'cotacoes']]);
-        Inflector::rules('plural', ['irregular' => ['item' => 'itens']]);
+        Inflector::rules('plural', ['irregular' => [
+            'cotacao' => 'cotacoes',
+            'item' => 'itens',
+            'enderecavel' => 'enderecaveis'
+        ]]);
     }
-
 }
