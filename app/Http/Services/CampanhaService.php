0849bd057261b36972001e21107cdb12453f5103
Flávio Souza dos santos <flaviosantos.dev@gmail.com>
2020-11-20 10:44:52 +0000

    Esqueleto do serviço de campanhas

---
+++ app/Http/Services/CampanhaService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Services;
+
+class CampanhaService
+{
+    public function getCampanhas($paginate = 10)
+    {
+        # code...
+    }
+
+    public function findCampanha($id)
+    {
+        # code...
+    }
+
+    public function saveCampanha($request)
+    {
+        # code...
+    }
+
+    public function updateCampanha($request, $id)
+    {
+        # code...
+    }
+
+    public function deleteCampanha($id)
+    {
+        # code...
+    }
+}
