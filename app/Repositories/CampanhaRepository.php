05bc2abdecbd32d03efdeddd122cc0d63be92780
Flávio Souza dos santos <flaviosantos.dev@gmail.com>
2020-11-20 10:37:22 +0000

    Esqueleto do Repositório de Campanhas

---
+++ app/Repositories/CampanhaRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Campanha;
+
+class CampanhaRepository
+{
+    public function getCampanhas($paginate = 10)
+    {
+
+    }
+
+    public function findCampanha($id)
+    {
+
+    }
+
+    public function saveCampanha(Campanha $campanha)
+    {
+
+    }
+
+    public function updateCampanha($request, $id)
+    {
+
+    }
+
+    public function deleteCampanha($id)
+    {
+        
+    }
+}
